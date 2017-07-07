92f1e8202614830b2b28ceee9e07eb70b7f6bd43
xsetra <abdullahcaliskan511@gmail.com>
2017-07-07 21:14:37 +0300

    imgpack için on_post metodu eklendi

--- first_look/images.py
+++ first_look/images.py
@@ -1,11 +1,23 @@
 # -*-  coding: utf-8 -*-
 import json
 import falcon
+import msgpack
+import io
 
 class Resource(object):
 
-    def on_get(self, req, resp): # Bu methodlara responder deniliyormuş
-        doc = { 'images':[{'href':'/images/somethings'}] }
+    def on_get(self, req, resp): # Responder with JSON
+        doc = {
+            'images':[
+                {'href2':'/images/somethings'}
+            ]
+        }
         # Json representation of the resource
         resp.body = json.dumps(doc, ensure_ascii=False)
+        resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp): # Responder with MSGPACK
+        doc = {'images':[ {'href':'/images/something'} ]}
+        resp.data = msgpack.packb(doc, use_bin_type=True)
+        resp.content_type = 'application/msgpack'
         resp.status = falcon.HTTP_200
