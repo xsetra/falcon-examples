a61a75f721e5c5480991a41c53b25c3161a1a799
xsetra <abdullahcaliskan511@gmail.com>
2017-07-07 22:02:15 +0300

    Hello için resources ve routing işlemleri yapıldı. Gunicorn, http ve curl komutları yazıldı

--- hello/app.py
+++ hello/app.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+
+import falcon
+
+class Hello(object):
+    def on_get(self, request, response):
+        response.body = "Hello"
+        response.status = falcon.HTTP_200
+
+    def on_post(self, request, response):
+        response.body = "Hello via POST method"
+        response.status = falcon.HTTP_200
+
+# application variable name is needed for Gunicorn wsgi web server.
+app = application = falcon.API()
+app.add_route('/', Hello())
+
+"""
+gunicorn hello.app --reload
+
+http POST 127.0.0.1:8000/
+curl -X POST 127.0.0.1:8000/
+
+"""
