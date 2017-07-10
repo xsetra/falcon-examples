a9da58045812c2c18fbb396c968819c810a0528b
xsetra <abdullahcaliskan511@gmail.com>
2017-07-10 11:19:24 +0300

    Images TDD için saved images yazıldı

--- first_look/test_app.py
+++ first_look/test_app.py
@@ -4,7 +4,15 @@ import falcon
 from falcon import testing
 import msgpack
 from .app import api
+from unittest.mock import mock_open, call
+import pytest
 
+
+@pytest.fixture
+def client():
+    return testing.TestClient(api)
+
+test_client = client()
 
 def test_list_images():
     expected_response = {
@@ -12,10 +20,28 @@ def test_list_images():
                 {'href2':'/images/somethings'}
             ]
         }
-    test_client = testing.TestClient(api)
 
     response = test_client.simulate_get('/images')
     response_result = msgpack.unpackb(response.content, encoding='utf-8')
 
     assert response_result == expected_response
     assert response.status == falcon.HTTP_OK
+
+
+def test_posted_image_gets_saved(test_client, monkeypatch):
+    mock_file_open = mock_open()
+    monkeypatch.setattr('io.open', mock_file_open)
+
+    fake_uuid = '123e4567-e89b-12d3-a456-426655440000'
+    monkeypatch.setattr('uuid.uuid4', lambda: fake_uuid)
+
+    fake_image_bytes = b'fake-image-bytes'
+    response = test_client.simulate_post(
+        '/images',
+        body=fake_image_bytes,
+        headers={'content-type': 'image/png'}
+    )
+
+    assert response.status == falcon.HTTP_CREATED
+    assert call().write(fake_image_bytes) in mock_file_open.mock_calls
+    assert response.headers['location'] == '/images/{}.png'.format(fake_uuid)
